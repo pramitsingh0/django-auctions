5a74376cfb3377f6789cc7511b96ad4a33158e76
pramitsingh0 <pramitsingh0@pop-os.localdomain>
2021-10-31 15:53:44 +0530

    Completed AuctionListing model

--- auctions/models.py
+++ auctions/models.py
@@ -6,8 +6,15 @@ class User(AbstractUser):
     pass
 
 class AuctionListing(models.Model):
-    pass
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    starting_bid = models.IntegerField()
+    seller = models.CharField(max_length=64)
+    imglink = models.CharField(max_length=200, default=None)
+    category = models.CharField(max_length=64)
 
+    def __str__(self):
+        return f"{self.id}: {self.title}"
 class Bids(models.Model):
     pass
 
